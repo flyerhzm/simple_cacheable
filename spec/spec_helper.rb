bf870e7671c86aed69af5709b030624a7da790b3
Richard Huang <flyerhzm@gmail.com>
2013-07-07 22:58:57 +0800

    fix .all warning, use .to_a instead

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ require 'rspec'
 require 'mocha/api'
 require 'memcached'
 require 'cacheable'
-require 'debugger'
 
 # MODELS = File.join(File.dirname(__FILE__), "models")
 # $LOAD_PATH.unshift(MODELS)
