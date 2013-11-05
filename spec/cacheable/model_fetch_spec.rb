8948445940a3076b3af1c22a7cee9e1140290cae
pathouse <patmcgee331@gmail.com>
2013-11-05 12:42:11 -0500

    model fetch unit tests

--- spec/cacheable/model_fetch_spec.rb
+++ spec/cacheable/model_fetch_spec.rb
@@ -30,4 +30,33 @@ describe Cacheable do
  			end.should == [@post1, @post2]
  		end
  	end
+
+ 	describe "unit tests" do
+
+ 		describe "#write_to_cache" do
+ 			it "should write an encoded object to the cache" do
+ 				Cacheable::ModelFetch.send(:write_to_cache, user.model_cache_key, user)
+ 				Rails.cache.read(user.model_cache_key).should == { :class => user.class, 'attributes' => user.attributes }
+ 			end
+ 		end
+
+ 		describe "#read_from_cache" do
+ 			it "should decode an encoded object read from the cache" do
+ 				Rails.cache.write(user.model_cache_key, {:class => user.class, 'attributes' => user.attributes} )
+ 				Cacheable::ModelFetch.send(:read_from_cache, user.model_cache_key).should == user
+ 			end
+ 		end
+
+ 		describe "#coder_from_record" do
+ 			it "should encode an object" do
+ 				Cacheable::ModelFetch.send(:coder_from_record, user).should == {:class => user.class, 'attributes' => user.attributes}
+ 			end
+ 		end
+
+ 		describe "#record_from_coder" do
+ 			it "should decode an object" do
+ 				Cacheable::ModelFetch.send(:record_from_coder, {:class => user.class, 'attributes' => user.attributes}).should == user
+ 			end
+ 		end
+ 	end
 end
